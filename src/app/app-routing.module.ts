63a2eecfc74fba0ddb2500da0f8133a9a8780e67
Anubhav <proanubhav@gmail.com>
2022-05-30 22:31:24 +0530

    profile ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentComponent } from './admin/appointment/appointment.component';
+import { ChangePasswordComponent } from './admin/change-password/change-password.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { EditProfileComponent } from './admin/edit-profile/edit-profile.component';
+import { ProfileComponent } from './admin/profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -17,6 +21,22 @@ const routes: Routes = [
   {
     path: 'admin',
     component: DashboardComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfileComponent,
+  },
+  {
+    path: 'change-password',
+    component: ChangePasswordComponent,
+  },
+  {
+    path: 'appointment',
+    component: AppointmentComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { SidebarComponent } from './admin/sidebar/sidebar.component';
 import { ProfileComponent } from './admin/profile/profile.component';
+import { ChangePasswordComponent } from './admin/change-password/change-password.component';
+import { EditProfileComponent } from './admin/edit-profile/edit-profile.component';
+import { AppointmentComponent } from './admin/appointment/appointment.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { ProfileComponent } from './admin/profile/profile.component';
     DashboardComponent,
     SidebarComponent,
     ProfileComponent,
+    ChangePasswordComponent,
+    EditProfileComponent,
+    AppointmentComponent,
   ],
   imports: [
     BrowserModule,
